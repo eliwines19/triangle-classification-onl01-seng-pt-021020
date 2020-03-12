4050d2767eb6231134fec2e5a39e2f44f7a75ab8
Elijah wines <elijahwines84@yahoo.com>
2020-03-12 14:34:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,11 @@
 class Triangle
 
-  def initialize(s_one_l, s_two_l, s_three_l)
-
+  def initialize(side_1, side_2, side_3)
+    @triangle_sides = []
+    @triangle_sides << side_1 
+    @triangle_sides << side_2
+    @triangle_sides << side_3
   end
+
+  
 end
